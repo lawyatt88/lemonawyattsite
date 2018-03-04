48c6e28bf6bef20988d74b6fb3e006ca525e1b73
LeMona Wyatt <lawyatt88@gmail.com>
2018-03-04 14:38:16 -0500

    added express-favicon

--- server/index.js
+++ server/index.js
@@ -1,38 +1,7 @@
 'use strict'; 
-
-// const express = require('express');
-// const path = require('path');
-// const volleyball = require('volleyball');
-// const bodyParser = require('body-parser');
-
-// const app = express();
-
-// //logging middleware
-// app.use(volleyball);
-
-// //body parsing middleware
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// //static middleware
-// app.use(express.static(path.join(__dirname, '../public')));
-
-// app.use('/api', require('./api')); // include our routes!
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../public/index.html'));
-// }); // Send index.html for any other requests
-
-// //error handling middleware
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(err.status || 500).send(err.message || 'Internal server error');
-// });
-
-// module.exports = app;
-
 const path = require('path')
 const express = require('express')
+const favicon = require('express-favicon');
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const compression = require('compression')
@@ -68,6 +37,7 @@ const createApp = () => {
 
   // static file-serving middleware
   app.use(express.static(path.join(__dirname, '..', 'public')))
+  app.use(favicon(__dirname + '/public/favicon.ico'));
 
   // any remaining requests with an extension (.js, .css, etc.) send 404
   app.use((req, res, next) => {
